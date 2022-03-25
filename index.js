a6fdfa650a98201a62fa66dd15cb1a2eb6664543
Meet Gada <kjscelive@somaiya.edu>
2022-03-26 02:26:57 +0530

    styling of pages

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import * as THREE from './modules/three.module.js'
-import {GLTFLoader} from './modules/GLTFLoader.js'
-import {GUI} from './modules/dat.gui.module.js'
-import {OrbitControls} from './modules/OrbitControls.js'
-import Stats from './modules/stats.module.js'
+import * as THREE from './three.js-dev/build/three.module.js'
+import {GLTFLoader} from './three.js-dev/examples/jsm/loaders/GLTFLoader.js'
+import {GUI} from './three.js-dev/dat.gui.module.js'
+import {OrbitControls} from './three.js-dev/examples/jsm/controls/OrbitControls.js'
+import Stats from './three.js-dev/examples/jsm/libs/stats.module.js'
 
 const canvas = document.querySelector('.webgl')
 const mybody = document.querySelector('.canvas-container')
@@ -12,24 +12,38 @@ const scene = new THREE.Scene()
 // const gui = new GUI()  //enable this to use dat gui
 // const gltfFolder = gui.addFolder("Model")
 
+//working model
+// const loader = new GLTFLoader()
+// loader.load('gltf/gltf/wraith.gltf', function(gltf){
+//     console.log(gltf)
+//     const root = gltf.scene
+//     gltfFolder.add(root.position,"x",-50,50,0.1).name("X-position")
+//     gltfFolder.add(root.position,"y",-50,50,0.1).name("Y-position")
+//     gltfFolder.add(root.position,"z",-50,50,0.1).name("Z-position")
+//     root.scale.set(0.1,0.1,0.1)
+//     scene.add(root)
+// },function(xhr){
+//     console.log((xhr.loaded/xhr.total*100)+"% loaded")
+// }, function(error){
+//     console.log('An Error Occured')
+// })
 
-const loader = new GLTFLoader()
-loader.load('land/land.glb', function(gltf){
-    console.log(gltf)
-    const root = gltf.scene
-    root.rotation.y = 1.15
-    root.scale.set(0.1,0.1,0.1)
-    scene.add(root)
-    //console.log(root.name)
-},function(xhr){
-    console.log((xhr.loaded/xhr.total*100)+"% loaded")
-}, function(error){
-    console.log('An Error Occured')
-})
-
+// const loader = new GLTFLoader()
+// loader.load('land/land.glb', function(gltf){
+//     console.log(gltf)
+//     const root = gltf.scene
+//     root.rotation.y = 1.15
+//     root.scale.set(0.1,0.1,0.1)
+//     scene.add(root)
+//     //console.log(root.name)
+// },function(xhr){
+//     console.log((xhr.loaded/xhr.total*100)+"% loaded")
+// }, function(error){
+//     console.log('An Error Occured')
+// })
 
 const loader1 = new GLTFLoader()
-loader1.load('land/land_left.glb', function(gltf){
+loader1.load('land/new.glb', function(gltf){
     console.log(gltf)
     const root = gltf.scene
     root.rotation.y = 1.15
@@ -42,43 +56,64 @@ loader1.load('land/land_left.glb', function(gltf){
 })
 loader1.castShadow=true
 
-const loader2 = new GLTFLoader()
-loader2.load('land/land_center.glb', function(gltf){
-    console.log(gltf)
-    const root = gltf.scene
-    root.rotation.y = 1.15
-    root.scale.set(0.1,0.1,0.1)
-    scene.add(root)
-},function(xhr){
-    console.log((xhr.loaded/xhr.total*100)+"% loaded")
-}, function(error){
-    console.log('An Error Occured')
-})
+// const loader1 = new GLTFLoader()
+// loader1.load('land/land_left.glb', function(gltf){
+//     console.log(gltf)
+//     const root = gltf.scene
+//     root.rotation.y = 1.15
+//     root.scale.set(0.1,0.1,0.1)
+//     scene.add(root)
+// },function(xhr){
+//     console.log((xhr.loaded/xhr.total*100)+"% loaded")
+// }, function(error){
+//     console.log('An Error Occured')
+// })
+// loader1.castShadow=true
 
-const loader3 = new GLTFLoader()
-loader3.load('land/land_right.glb', function(gltf){
-    console.log(gltf)
-    const root = gltf.scene
-    root.rotation.y = 1.15
-    root.scale.set(0.1,0.1,0.1)
-    scene.add(root)
-},function(xhr){
-    console.log((xhr.loaded/xhr.total*100)+"% loaded")
-}, function(error){
-    console.log('An Error Occured')
-})
+// const loader2 = new GLTFLoader()
+// loader2.load('land/land_center.glb', function(gltf){
+//     console.log(gltf)
+//     const root = gltf.scene
+//     root.rotation.y = 1.15
+//     root.scale.set(0.1,0.1,0.1)
+//     scene.add(root)
+// },function(xhr){
+//     console.log((xhr.loaded/xhr.total*100)+"% loaded")
+// }, function(error){
+//     console.log('An Error Occured')
+// })
+
+// const loader3 = new GLTFLoader()
+// loader3.load('land/land_right.glb', function(gltf){
+//     console.log(gltf)
+//     const root = gltf.scene
+//     root.rotation.y = 1.15
+//     root.scale.set(0.1,0.1,0.1)
+//     scene.add(root)
+// },function(xhr){
+//     console.log((xhr.loaded/xhr.total*100)+"% loaded")
+// }, function(error){
+//     console.log('An Error Occured')
+// })
 
 // scene.add( new THREE.AxesHelper(500))
 
-const light = new THREE.DirectionalLight(0xffffff,10)
+const light = new THREE.DirectionalLight(0xffffff,1)
 light.position.set(2,2,5)
 light.castShadow = true;
 scene.add(light)
 
-const light1 = new THREE.DirectionalLight(0xffffff,10)
+const light1 = new THREE.DirectionalLight(0xffffff,1)
 light1.position.set(-2,-2,5)
 light1.castShadow = true;
 scene.add(light1)
+
+// const geometry = new THREE.BoxGeometry(1,1,1)
+// const material = new THREE.MeshBasicMaterial({
+//     color: 'red'
+// })
+// const boxMesh = new THREE.Mesh(geometry,material)
+// scene.add(boxMesh)
 
 //Boiler Plate Code
 const sizes = {
@@ -97,6 +132,7 @@ mybody.appendChild( stats.dom );
 
 const raycaster = new THREE.Raycaster()
 const clickMouse = new THREE.Vector2()
+const moveMouse = new THREE.Vector2()
 var draggable = new THREE.Object3D()
 var data
 
